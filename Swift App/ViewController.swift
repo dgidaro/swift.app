6ff8fa5a759aac3c7ea07c76d1483c37e2ff5ce9
Daniel Gidaro <danielgidaro@Daniels-MacBook-Pro.local>
2016-12-18 16:17:44 -0500

    Initial Commit

---
+++ Swift App/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Swift App
+//
+//  Created by Daniel Gidaro on 12/18/16.
+//  Copyright © 2016 Daniel Gidaro. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
